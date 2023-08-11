e2f5e14891f680f3f696b57a889b79184b680283
Nicolas Bottarini <nicolasbottarini@gmail.com>
2023-08-10 22:06:19 -0300

    Add validations and loader support to PresenterBase

--- src/PresenterBase.ts
+++ src/PresenterBase.ts
@@ -1,4 +1,5 @@
 import { ChangeFunc } from './ChangeFunc'
+import { Validator } from '@nbottarini/validator'
 
 export abstract class PresenterBase<TModel = any> {
     protected _model: TModel
@@ -14,5 +15,49 @@ export abstract class PresenterBase<TModel = any> {
     protected updateModel<K extends keyof TModel>(changes: Pick<TModel, K>): void {
         this._model = { ...this.model, ...changes }
         this.onChange(this._model)
+    }
+
+    protected validateModel(): boolean {
+        if (typeof this.model !== 'object' || !('errors' in this.model)) throw new Error('Unsupported model')
+        const validator = new Validator()
+        this.validations(validator)
+        const isValid = !validator.hasErrors()
+        if (!isValid) {
+            // @ts-ignore
+            this.updateModel({ errors: validator.allErrorMessages() })
+        }
+        return isValid
+    }
+
+    protected validations(validator: Validator) {
+    }
+
+    protected showError(propertyName: string, message: string) {
+        if (typeof this.model !== 'object' || !('errors' in this.model) || !(this.model.errors instanceof Object)) {
+            throw new Error('Unsupported model')
+        }
+        // @ts-ignore
+        this.updateModel({ errors: { [propertyName]: message } })
+    }
+
+    protected showGeneralError(message: string) {
+        this.showError('general', message)
+    }
+
+    protected resetErrors() {
+        // @ts-ignore
+        this.updateModel({ errors: {} })
+    }
+
+    protected async withLoader(block: () => Promise<any>) {
+        if (typeof this.model !== 'object' || !('isLoading' in this.model)) throw new Error('Unsupported model')
+        // @ts-ignore
+        this.updateModel({ isLoading: true })
+        try {
+            await block()
+        } finally {
+            // @ts-ignore
+            this.updateModel({ isLoading: false })
+        }
     }
 }
